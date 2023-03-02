6229e6d8f8e31f7baac4b722f00eab6379880ea5
Reiyirak <pdsmej@gmail.com>
2023-03-01 18:41:25 -0600

    mend

---
+++ src/components/filternav/Filternav.jsx
@@ -0,0 +1,29 @@
+import './Filternav.css' 
+
+export default function navBar() {
+  return (
+    <div class="py-3 mt-1 -mx-3 overflow-y-auto whitespace-nowrap scroll-hidden">
+    <a class="filter" href="#">Casas de Campo</a>
+    <a class="filter" href="#">Castillos</a>
+    <a class="filter" href="#">Populares</a>
+    <a class="filter" href="#">Golf</a>
+    <a class="filter" href="#">Lago</a>
+    <a class="filter" href="#">Habitaciones privadas</a>
+    <a class="filter" href="#">Frente a la playa</a>
+    <a class="filter" href="#">Albercas increibles</a>
+    <a class="filter" href="#">Cabañas</a>
+    <a class="filter" href="#">Mansiones</a>
+    <a class="filter" href="#">Vista increible</a>
+    <a class="filter" href="#">Tropical</a>
+    <a class="filter" href="#">Extraordinarios</a>
+    <a class="filter" href="#">Casas domo</a>
+    <a class="filter" href="#">Minicasas</a>
+    <a class="filter" href="#">En el lago</a>
+    <a class="filter" href="#">Casas de arbol</a>
+    <a class="filter" href="#">Islas</a>
+    <a class="filter" href="#">Parques nacionales</a>
+    <a class="filter" href="#">Luxe</a>
+    </div>
+  )
+}
+
