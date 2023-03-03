bdeb4781549c1ed366ce6ffe5f05df529052832c
Reiyirak <pdsmej@gmail.com>
2023-03-03 17:29:01 -0600

    Fixed Conflicts
    The was a conflict between my Homepage.js and the Homepage.js of master.
    In this commit i fixed that.

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -2,6 +2,7 @@ import './Homepage.css';
 import { Link } from "react-router-dom";
 import NavBar from "../components/nabvar/Nabvar"
 import FilterNav from "../components/filternav/Filternav.jsx"
+import Cards from "../components/Cards/index"
 
 function Homepage() {
   return (
