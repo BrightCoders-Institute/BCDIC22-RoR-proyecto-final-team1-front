cc66114516c7bec8fdccedbc446a2577d1e1259a
Yaxkin121 <ycardenas13@ucol.mx>
2023-03-04 14:50:02 -0600

    Release

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,16 +9,27 @@ export default function footer() {
     <footer className="footer-bottom">
       <div className='row'>
         <div className='col-md-6'>
-        <p className='flex data-f-1'>© 2023 Airbnb, Inc. · Privacidad · Términos · Mapa del sitio · Información de la compañía</p>
+          <p className='flex data-f-1'>
+            © 2023 Airbnb, Inc. · 
+            <button className='margin-b'>Privacidad</button> 
+            · 
+            <button className='margin-b'>Términos</button> 
+            · 
+            <button className='margin-b'>Mapa del sitio</button>
+            · 
+            <button className='margin-c'>Información de la compañía</button>
+          </p>
         </div>
         <div className='col-md-6'>
           <p className='flex data-f-2'>
-            <GlobeAltIcon class="up-icon h-6 w-6 text-black-500" />
-            Español(MX) $ MXN Ayuda y recursos
-            <ChevronUpIcon className="up-icon h-5 w-5 text-black-500"/></p>
+            <button className='margin-a'><GlobeAltIcon class="h-5 w-6 text-black-500" /></button>
+            <button className='margin-b'> Español (MX) </button>
+            <button className='margin-b'> $ MXN </button> 
+            <button className='margin-b'> Ayuda y recursos </button>
+            <button><ChevronUpIcon className="up-icon h-6 w-5 text-black-500"/></button>
+          </p>
         </div>
       </div>
     </footer>
   );
 }
-/* push test */
