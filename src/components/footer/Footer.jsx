fcb72a0236cb32700cfddcfd569a85fd7db262d4
Yaxkin121 <ycardenas13@ucol.mx>
2023-03-04 13:21:23 -0600

    pre-release

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,26 +1,23 @@
 import React from 'react';
 import './Footer.css'
 import { ChevronUpIcon } from "@heroicons/react/24/outline";
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
 // import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 export default function footer() {
   return (
-    <footer className="bg-red-200 footer-bottom">
-        <div>
-          <div className="row">
-            <div className="col-md-6">
-              <div className='align-left'>
-              <p>© 2023 Airbnb, Inc. · Privacidad · Términos · Mapa del sitio · Información de la compañía</p>
-              </div>
-            </div>
-            <div className="col-md-6">
-              <div className='flex justify-end'>
-              <p>Español (MX) · $ MXN · Ayuda y recursos</p>
-              <ChevronUpIcon className="h-6 w-6 text-gray-500"/> 
-              </div>
-            </div>
-          </div>
+    <footer className="footer-bottom">
+      <div className='row'>
+        <div className='col-md-6'>
+        <p className='flex data-f-1'>© 2023 Airbnb, Inc. · Privacidad · Términos · Mapa del sitio · Información de la compañía</p>
         </div>
-      </footer>
-    );
+        <div className='col-md-6'>
+          <p className='flex data-f-2'>
+            <GlobeAltIcon class="up-icon h-6 w-6 text-black-500" />
+            Español(MX) $ MXN Ayuda y recursos
+            <ChevronUpIcon className="up-icon h-5 w-5 text-black-500"/></p>
+        </div>
+      </div>
+    </footer>
+  );
 }
