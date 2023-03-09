5063d28ca0e521f81fd4ac7b3d9b823ca52a1a9a
Reiyirak <pdsmej@gmail.com>
2023-03-08 19:19:41 -0600

    Base design of the component
    I made the base structure of the login/sign-up component, where i will
    edit everything necessary for it to be as close as the mockup design.

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -4,14 +4,16 @@ import NavBar from "../components/nabvar/Nabvar"
 import Footer from "../components/footer/Footer"
 import FilterNav from "../components/filternav/Filternav.jsx"
 import Cards from "../components/Cards/index"
+import SignUser from '../components/signuser/Signuser'
 
 function Homepage() {
   return (
     <>
-    <NavBar />
-    <FilterNav />
+      <NavBar />
+      <FilterNav />
       <Cards />
-      <Footer />    
+      <SignUser />
+      <Footer />
     </>
   );
 }
