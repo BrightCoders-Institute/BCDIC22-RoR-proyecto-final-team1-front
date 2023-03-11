a146a74f5ffa862ad3994a925af3dffb5cb55108
Yaxkin121 <ycardenas13@ucol.mx>
2023-03-10 21:20:09 -0600

    Progress v2

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -6,38 +6,42 @@ import { MdOutlineCalendarToday } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
 
 export default function footer() {
-    return (
-        <div class="columns-2 flex font">
-            <div className='w-3/5 divide-y'>
-                <div className=' column-2'>
-                    <div>
-                    <p className='bld'>
-                        Habitación privada en cabaña. Anfitrión: Ale Y Cris
-                    </p>
-                    <p>2 huéspedes1 habitación1 cama1 baño privado
-                    </p>
-                    </div>
-                    <div><CgProfile className="h-6 w-6 text-black-500 justify-center" /></div>
-                </div>
-                <div>
-                    <p className='bld'>
-                        <TrophyIcon className="h-6 w-6 text-black-500" />
-                        Ale Y Cris es SuperAnfitrión
-                    </p>
-                    <p>Los SuperAnfitriones son anfitriones experimentados, 
-                    con evaluaciones excelentes, que se esfuerzan al máximo por ofrecer estancias maravillosas.</p>
-                    <p className='bld'>
-                        <FaSwimmingPool className="h-6 w-6 text-black-500" />
-                        Sumérgete
-                    </p>
-                    <p>Este es uno de los pocos lugares en la zona con una alberca.</p>
-                <p className='bld'>
-                    <MdOutlineCalendarToday className="h-6 w-6 text-black-500" />
-                    Cancelación gratuita durante 48 horas.
-                </p>
-                </div>
-            </div>
-                <div className='w-2/5'>asd</div>
+  return (
+    <div class="body font">
+      <div className='fst-c'>
+        <div className='columns-2 flex'>
+          <div className='seller item-center'>
+            <p className='bld'>
+              Habitación privada en cabaña. Anfitrión: Ale Y Cris
+            </p>
+            <p>2 huéspedes · 1 habitación · 1 cama · 1 baño privado
+            </p>
+          </div>
+          <div className='seller-pp'>
+            <CgProfile className="icon-f" />
+          </div>
         </div>
-    );
+        <div>
+          <p className='bld'>
+            <TrophyIcon className="icon" />
+            Ale Y Cris es SuperAnfitrión
+          </p>
+          <p>Los SuperAnfitriones son anfitriones experimentados,
+            con evaluaciones excelentes, que se esfuerzan al máximo por ofrecer estancias maravillosas.</p>
+          <p className='bld'>
+            <FaSwimmingPool className="icon" />
+            Sumérgete
+          </p>
+          <p>Este es uno de los pocos lugares en la zona con una alberca.</p>
+          <p className='bld'>
+            <MdOutlineCalendarToday className="icon" />
+            Cancelación gratuita durante 48 horas.
+          </p>
+        </div>
+      </div>
+      <div className='snd-c'>
+        <p>asd</p>
+      </div>
+    </div>
+  );
 }
