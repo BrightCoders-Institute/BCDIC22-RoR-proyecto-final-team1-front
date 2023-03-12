b6d7639e1176e995020dd991564c2c2af7c1ae3c
Yaxkin121 <ycardenas13@ucol.mx>
2023-03-11 23:42:50 -0600

    Progress v3

--- src/components/details/Details.jsx
+++ src/components/details/Details.jsx
@@ -4,43 +4,75 @@ import { TrophyIcon } from "@heroicons/react/24/outline";
 import { FaSwimmingPool } from "react-icons/fa";
 import { MdOutlineCalendarToday } from "react-icons/md";
 import { CgProfile } from "react-icons/cg";
+import aircover from "../../assets/aircover.jpg";
 
 export default function footer() {
   return (
     <div class="body font">
       <div className='fst-c'>
-        <div className='columns-2 flex'>
-          <div className='seller item-center'>
-            <p className='bld'>
+        <div className='c-sty'>
+          <div className='seller'>
+            <p className='bld text-xl'>
               Habitación privada en cabaña. Anfitrión: Ale Y Cris
             </p>
-            <p>2 huéspedes · 1 habitación · 1 cama · 1 baño privado
+            <p className='text-base'>2 huéspedes · 1 habitación · 1 cama · 1 baño privado
             </p>
           </div>
           <div className='seller-pp'>
             <CgProfile className="icon-f" />
           </div>
         </div>
+        {/* reference space */}
+        {/* reference space */}
+        {/* reference space */}
+        {/* reference space */}
         <div>
-          <p className='bld'>
-            <TrophyIcon className="icon" />
-            Ale Y Cris es SuperAnfitrión
-          </p>
-          <p>Los SuperAnfitriones son anfitriones experimentados,
-            con evaluaciones excelentes, que se esfuerzan al máximo por ofrecer estancias maravillosas.</p>
-          <p className='bld'>
-            <FaSwimmingPool className="icon" />
-            Sumérgete
-          </p>
-          <p>Este es uno de los pocos lugares en la zona con una alberca.</p>
-          <p className='bld'>
-            <MdOutlineCalendarToday className="icon" />
+          <div className='pdg-y g-col'>
+            <div className='row-span-2'>
+              <TrophyIcon className="icon" />
+            </div>
+            <div className=''>
+              <p className='bld'>Ale Y Cris es SuperAnfitrión</p>
+              <p>Los SuperAnfitriones son anfitriones experimentados,
+                con evaluaciones excelentes, que se esfuerzan al máximo por ofrecer estancias maravillosas.</p>
+            </div>
+          </div>
+          {/* reference space */}
+          <div className='g-col'>
+            <div className='row-span-2'>
+              <FaSwimmingPool className="icon-m" />
+            </div>
+            <div className='spcn'>
+              <p className='bld'>Sumérgete</p>
+              <p>Este es uno de los pocos lugares en la zona con una alberca.</p>
+            </div>
+          </div>
+          {/* reference space */}
+          <p className='bld-l'>
+            <MdOutlineCalendarToday className="icon-l" />
             Cancelación gratuita durante 48 horas.
           </p>
         </div>
+        <div>
+        <img className="img-s" src={aircover} alt='aircover'/>
+        <p className='m-s'>Todas las reservaciones incluyen protección 
+          gratuita en caso de que el anfitrión cancele, 
+          de que haya imprecisiones en el anuncio o de 
+          que surjan otros inconvenientes, como problemas 
+          al momento de la llegada.
+        </p>
+        <button className='bld underline'>Más información.</button>
+        </div>
+        <div>
+          <p>MORE HERE</p>
+        </div>
       </div>
-      <div className='snd-c'>
-        <p>asd</p>
+      {/* reference space */}
+      {/* reference space */}
+      {/* reference space */}
+      {/* reference space */}
+      <div className=''>
+        <p className=''>Second Column</p>
       </div>
     </div>
   );
