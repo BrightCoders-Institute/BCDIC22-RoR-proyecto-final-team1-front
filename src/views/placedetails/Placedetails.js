1491e46141bd11e78e7a2ce3c6d04224735cb6bc
Severo Iglesias <siglesias@ucol.mx>
2023-03-13 15:44:18 -0600

    feature#20 reserve component
    component for the user to create a reservation for a place

--- src/views/placedetails/Placedetails.js
+++ src/views/placedetails/Placedetails.js
@@ -1,12 +1,13 @@
 import './Placedetails.css';
 import NavBar from "../../components/nabvar/Nabvar"
 import Footer from "../../components/footer/Footer"
+import Reserve from "../../components/reserve/reserve"
 
 function Placedetails() {
     return (
     <>
     <NavBar />
-
+		<Reserve />
     <Footer />    
     </>
     );
