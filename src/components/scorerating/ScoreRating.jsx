5d62245466aa214384d7e2da3b4746ad58004d2a
Reiyirak <pdsmej@gmail.com>
2023-03-13 18:17:48 -0600

    Delete a lot of divs and made loop
    I deleted most divs from the score rating component and instead
    made a map where each element of the list will be displayed.

--- src/components/scorerating/ScoreRating.jsx
+++ src/components/scorerating/ScoreRating.jsx
@@ -1,6 +1,18 @@
 import { AiFillStar } from "react-icons/ai"
 import './scorerating.css'
 
+const scoreRow1 = [
+  { id: 1, name: "Limpieza", score: 4 },
+  { id: 2, name: "Comunicación", score: 4 },
+  { id: 3, name: "Llegada", score: 4 },
+];
+
+const scoreRow2 = [
+  { id: 1, name: "Precisión", score: 4 },
+  { id: 2, name: "Ubicación", score: 4 },
+  { id: 3, name: "Calidad-precio", score: 4 },
+];
+
 export default function scoreRating() {
   return (
     <>
@@ -11,64 +23,38 @@ export default function scoreRating() {
         <span class="w-1 h-1 mx-2 bg-gray-900 rounded-full dark:bg-dark"></span>
         <p class="text-xl font-medium text-dark dark:text-dark">376 reviews</p>
       </div>
+
       <div class="gap-28 sm:grid sm:grid-cols-2 px-8 scores">
         <div>
-          <dl class="flex flex-row">
-            <dt class="flex flex-1 text-lg font-medium text-dark dark:text-dark">Limpieza</dt>
-            <dd class="flex items-center justify-end flex-1 mb-3">
-              <div class="w-2/5 bg-gray-200 rounded h-1 dark:bg-gray-200 mr-2">
-                <div class="bg-gray-900 h-1 rounded dark:bg-gray-900" style={{ width: "89%" }}></div>
-              </div>
-              <span class="text-lg font-medium text-dark dark:text-dark">8.8</span>
-            </dd>
-          </dl>
-          <dl class="flex flex-row">
-            <dt class="flex flex-1 text-lg font-medium text-dark dark:text-dark">Comunicación</dt>
-            <dd class="flex items-center justify-end flex-1 mb-3">
-              <div class="w-2/5 bg-gray-200 rounded h-1 dark:bg-gray-200 mr-2">
-                <div class="bg-gray-900 h-1 rounded dark:bg-gray-900" style={{ width: "89%" }}></div>
-              </div>
-              <span class="text-lg font-medium text-dark dark:text-dark">8.8</span>
-            </dd>
-          </dl>
-          <dl class="flex flex-row">
-            <dt class="flex flex-1 text-lg font-medium text-dark dark:text-dark">Llegada</dt>
-            <dd class="flex items-center justify-end flex-1 mb-3">
-              <div class="w-2/5 bg-gray-200 rounded h-1 dark:bg-gray-200 mr-2">
-                <div class="bg-gray-900 h-1 rounded dark:bg-gray-900" style={{ width: "89%" }}></div>
-              </div>
-              <span class="text-lg font-medium text-dark dark:text-dark">8.8</span>
-            </dd>
-          </dl>
+
+          {scoreRow1.map(option => (
+            <dl class="flex flex-row">
+              <dt class="flex flex-1 text-lg font-medium text-dark dark:text-dark">{option.name}</dt>
+              <dd class="flex items-center justify-end flex-1 mb-3">
+                <div class="w-2/5 bg-gray-200 rounded h-1 dark:bg-gray-200 mr-2">
+                  <div class="bg-gray-900 h-1 rounded dark:bg-gray-900" style={{ width: "89%" }}></div>
+                </div>
+                <span class="text-lg font-medium text-dark dark:text-dark">{option.score}</span>
+              </dd>
+            </dl>
+          ))}
+
         </div>
+
         <div>
-          <dl class="flex flex-row">
-            <dt class="flex flex-1 text-lg font-medium text-dark dark:text-dark">Precisión</dt>
-            <dd class="flex items-center justify-end flex-1 mb-3">
-              <div class="w-2/5 bg-gray-200 rounded h-1 dark:bg-gray-200 mr-2">
-                <div class="bg-gray-900 h-1 rounded dark:bg-gray-900" style={{ width: "89%" }}></div>
-              </div>
-              <span class="text-lg font-medium text-dark dark:text-dark">8.8</span>
-            </dd>
-          </dl>
-          <dl class="flex flex-row">
-            <dt class="flex flex-1 text-lg font-medium text-dark dark:text-dark">Ubicación</dt>
-            <dd class="flex items-center justify-end flex-1 mb-3">
-              <div class="w-2/5 bg-gray-200 rounded h-1 dark:bg-gray-200 mr-2">
-                <div class="bg-gray-900 h-1 rounded dark:bg-gray-900" style={{ width: "89%" }}></div>
-              </div>
-              <span class="text-lg font-medium text-dark dark:text-dark">8.8</span>
-            </dd>
-          </dl>
-          <dl class="flex flex-row">
-            <dt class="flex flex-1 text-lg font-medium text-dark dark:text-dark">Calidad-Precio</dt>
-            <dd class="flex items-center justify-end flex-1 mb-3">
-              <div class="w-2/5 bg-gray-200 rounded h-1 dark:bg-gray-200 mr-2">
-                <div class="bg-gray-900 h-1 rounded dark:bg-gray-900" style={{ width: "89%" }}></div>
-              </div>
-              <span class="text-lg font-medium text-dark dark:text-dark">8.8</span>
-            </dd>
-          </dl>
+
+          {scoreRow2.map(option => (
+            <dl class="flex flex-row">
+              <dt class="flex flex-1 text-lg font-medium text-dark dark:text-dark">{option.name}</dt>
+              <dd class="flex items-center justify-end flex-1 mb-3">
+                <div class="w-2/5 bg-gray-200 rounded h-1 dark:bg-gray-200 mr-2">
+                  <div class="bg-gray-900 h-1 rounded dark:bg-gray-900" style={{ width: "89%" }}></div>
+                </div>
+                <span class="text-lg font-medium text-dark dark:text-dark">{option.score}</span>
+              </dd>
+            </dl>
+          ))}
+
         </div>
       </div>
 
