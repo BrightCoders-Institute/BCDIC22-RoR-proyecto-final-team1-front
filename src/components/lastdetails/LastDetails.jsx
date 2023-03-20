7b36a889360a8dd0a03e6a2e5de3a72f849e1b58
Reiyirak <pdsmej@gmail.com>
2023-03-20 01:21:50 -0600

    Finished bottom half of component

--- src/components/lastdetails/LastDetails.jsx
+++ src/components/lastdetails/LastDetails.jsx
@@ -1,209 +1,49 @@
+import { IoIosArrowForward } from 'react-icons/io'
+
+import './LastDetails.css'
 
 export default function() {
   return (
     <>
-      <section class="text-dark mb-16">
-        <div class="max-w-screen-xl px-4 py-4 sm:py-8 sm:px-6 lg:py-12 lg:px-8">
-          <div
-            class="mt-2 grid grid-cols-1 gap-8 md:mt-16 md:grid-cols-2 md:gap-12 lg:grid-cols-3"
-          >
-            <div class="flex items-start gap-4">
-              <span class="shrink-0 rounded-lg bg-gray-800 p-4">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                  <path
-                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                  ></path>
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  ></path>
-                </svg>
-              </span>
-
+      <section className="text-dark mb-16">
+        <div className="max-w-screen-xl px-4 py-2 sm:py-4 sm:px-6 lg:py-8 lg:px-8">
+          <h2 className="text-2xl text-dark font-bold">Que debes de saber</h2>
+          <div className="grid grid-cols-1 gap-8 md:mt-6 md:grid-cols-2 md:gap-12 lg:grid-cols-3">
+            <div className="flex items-start gap-4">
               <div>
-                <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
+                <h2 className="text-lg font-bold">Reglas de la casa</h2>
+                <p className="mt-2 text-sm">Horario de llegada: de las 13:00 a las 20:00</p>
+                <p className="mt-2 text-sm">Salida antes de las 10:00</p>
+                <p className="mt-2 text-sm">6 Huespedes como maximo</p>
+                <a href="#" class="arrow">Mostrar más <IoIosArrowForward /> </a>
 
-                <p class="mt-1 text-sm text-dark">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                  cumque tempore est ab possimus quisquam reiciendis tempora animi!
-                  Quaerat, saepe?
-                </p>
               </div>
             </div>
 
-            <div class="flex items-start gap-4">
-              <span class="shrink-0 rounded-lg bg-gray-800 p-4">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                  <path
-                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                  ></path>
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  ></path>
-                </svg>
-              </span>
-
+            <div className="flex items-start gap-4">
               <div>
-                <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
+                <h2 className="text-lg font-bold">Seguridad y propiedad</h2>
+                <p className="mt-2 text-sm">Lago, rio u otro cuerpo de agua cercano</p>
+                <p className="mt-2 text-sm">Estructura para trepar o jugar</p>
+                <p className="mt-2 text-sm">Alarma de monoxido de carbono</p>
+                <a href="#" class="arrow">Mostrar más <IoIosArrowForward /> </a>
 
-                <p class="mt-1 text-sm text-dark">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                  cumque tempore est ab possimus quisquam reiciendis tempora animi!
-                  Quaerat, saepe?
-                </p>
               </div>
             </div>
 
-            <div class="flex items-start gap-4">
-              <span class="shrink-0 rounded-lg bg-gray-800 p-4">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                  <path
-                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                  ></path>
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  ></path>
-                </svg>
-              </span>
-
+            <div className="flex items-start gap-4">
               <div>
-                <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-                <p class="mt-1 text-sm text-dark">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                  cumque tempore est ab possimus quisquam reiciendis tempora animi!
-                  Quaerat, saepe?
+                <h2 className="text-lg font-bold">Politica de cancelacion</h2>
+                <p className="mt-2 text-sm">Si cancelas antes del 27 de jun., recibiras un reembolso parcial.</p>
+                <p className="mt-2 text-sm">
+                  Consulta la politica de cancelacion completa del anfition, que se aplica incluso si cancelas haber contraido
+                  COVID-19 o por cualquier problema relacionado con el coronavirus.
                 </p>
+                <a href="#" class="arrow">Mostrar más <IoIosArrowForward /> </a>
+
               </div>
             </div>
 
-            <div class="flex items-start gap-4">
-              <span class="shrink-0 rounded-lg bg-gray-800 p-4">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                  <path
-                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                  ></path>
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  ></path>
-                </svg>
-              </span>
-
-              <div>
-                <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-                <p class="mt-1 text-sm text-dark">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                  cumque tempore est ab possimus quisquam reiciendis tempora animi!
-                  Quaerat, saepe?
-                </p>
-              </div>
-            </div>
-
-            <div class="flex items-start gap-4">
-              <span class="shrink-0 rounded-lg bg-gray-800 p-4">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                  <path
-                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                  ></path>
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  ></path>
-                </svg>
-              </span>
-
-              <div>
-                <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-                <p class="mt-1 text-sm text-dark">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                  cumque tempore est ab possimus quisquam reiciendis tempora animi!
-                  Quaerat, saepe?
-                </p>
-              </div>
-            </div>
-
-            <div class="flex items-start gap-4">
-              <span class="shrink-0 rounded-lg bg-gray-800 p-4">
-                <svg
-                  class="h-5 w-5"
-                  fill="none"
-                  stroke="currentColor"
-                  viewBox="0 0 24 24"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-                  <path
-                    d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-                  ></path>
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-                  ></path>
-                </svg>
-              </span>
-
-              <div>
-                <h2 class="text-lg font-bold">Lorem, ipsum dolor.</h2>
-
-                <p class="mt-1 text-sm text-dark">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Error
-                  cumque tempore est ab possimus quisquam reiciendis tempora animi!
-                  Quaerat, saepe?
-                </p>
-              </div>
-            </div>
           </div>
         </div>
       </section>
--- src/components/ownerdetails/OwnerDetails.jsx
+++ src/components/ownerdetails/OwnerDetails.jsx
@@ -4,12 +4,12 @@ export default function ownerDetails() {
   return (
     <>
 
-      <section class="text-dark border-b-gray-200 border-b-2">
-        <div class="max-w-screen-xl px-4 py-2 sm:py-4 sm:px-6 lg:py-8 lg:px-8">
-          <div class="max-w-xl">
-            <h2 class="text-3xl font-bold sm:text-4xl">What makes us special</h2>
+      <section className="text-dark border-b-gray-200 border-b-2">
+        <div className="max-w-screen-xl px-4 py-2 sm:py-4 sm:px-6 lg:py-8 lg:px-8">
+          <div className="max-w-xl">
+            <h2 className="text-3xl font-bold sm:text-4xl">What makes us special</h2>
 
-            <p class="mt-4 text-dark">
+            <p className="mt-4 text-dark">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat
               dolores iure fugit totam iste obcaecati. Consequatur ipsa quod ipsum
               sequi culpa delectus, cumque id tenetur quibusdam, quos fuga minima.
