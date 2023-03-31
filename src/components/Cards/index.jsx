b1df1d8fe88bde7d5a355d1f800f9c323331d1a7
Severo Iglesias <siglesias@ucol.mx>
2023-03-31 17:05:04 -0600

    add method await

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
 import { list } from '../../assets/cards-list';
 import Card from './card'
 import './card.css'
 
 export default function Cards() {
+	const [place, setPlace] = useState([]);
+
+	const getApi = async () => {
+		try {
+			const res = await fetch('https://rickandmortyapi.com/api/character');
+			const data = await res.json();
+
+			console.log(data.results);
+
+		} catch (err) {
+			console.error(err);
+		}
+	};
+	useEffect(() => {
+	getApi();
+}, [])
 	return (
 		<div className="cards-flex">
       {list.map((card, i) => (
