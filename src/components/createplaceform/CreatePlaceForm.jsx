5b8175a924941e1e499cbdb7207a811971808b1e
Reiyirak <pdsmej@gmail.com>
2023-04-10 23:38:11 -0600

    Finished design now time for functionality

--- src/components/createplaceform/CreatePlaceForm.jsx
+++ src/components/createplaceform/CreatePlaceForm.jsx
@@ -1,7 +1,6 @@
 import './CreatePlaceForm.css'
 
 const CreatePlaceForm = () => {
-
   return (
     <>
       <div id="background" aria-hidden="true" className="overflow-y-auto overflow-x-hidden justify-center items-center w-full md:inset-0 h-modal md:h-full">
@@ -9,40 +8,49 @@ const CreatePlaceForm = () => {
           <div className="relative p-4 rounded-lg shadow sm:p-5">
             <div className="flex justify-between items-center pb-4 mb-4 rounded-t border-b sm:mb-5 dark:border-gray-600">
               <h3 className="text-lg font-semibold text-white dark:text-dark">
-                Add Product
+                Añadir lugar
               </h3>
               <button type="button" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-white rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-dark" data-modal-toggle="defaultModal">
                 <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                <span className="sr-only">Close modal</span>
               </button>
             </div>
             <form action="#">
               <div className="grid gap-4 mb-4 sm:grid-cols-2">
                 <div>
-                  <label for="name" className="block mb-2 text-sm font-medium text-white dark:text-dark">Name</label>
-                  <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type product name" required="" />
+                  <label for="name" className="block mb-2 text-sm font-medium text-white dark:text-dark">Nombre</label>
+                  <input type="text" name="name" id="name" className="input-text focus:ring-primary-600 focus:border-primary-600" placeholder="Nombre del lugar" required="" />
                 </div>
                 <div>
-                  <label for="brand" className="block mb-2 text-sm font-medium text-white dark:text-dark">Brand</label>
-                  <input type="text" name="brand" id="brand" className="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Product brand" required="" />
+                  <label for="brand" className="block mb-2 text-sm font-medium text-white dark:text-dark">Dueño</label>
+                  <input type="text" name="brand" id="brand" className="input-text focus:ring-primary-600 focus:border-primary-600" placeholder="Pancho" required="" readOnly={true} />
                 </div>
                 <div>
-                  <label for="price" className="block mb-2 text-sm font-medium text-white dark:text-dark">Price</label>
-                  <input type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="$2999" required="" />
+                  <label for="price" className="block mb-2 text-sm font-medium text-white dark:text-dark">Precio por noche</label>
+                  <input type="number" name="price" id="price" className="input-text focus:ring-primary-600 focus:border-primary-600" placeholder="$2999" required="" />
                 </div>
                 <div>
-                  <label for="category" className="block mb-2 text-sm font-medium text-white dark:text-dark">Category</label>
+                  <label for="category" className="block mb-2 text-sm font-medium text-white dark:text-dark">Ciudad</label>
                   <select id="category" className="bg-gray-50 border border-gray-300 text-white text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                    <option selected="">Select category</option>
-                    <option value="TV">TV/Monitors</option>
-                    <option value="PC">PC</option>
-                    <option value="GA">Gaming/Console</option>
-                    <option value="PH">Phones</option>
+                    <option selected="">Selecciona ciudad</option>
+                    <option value="TV">Colima</option>
+                    <option value="PC">Guadalajara</option>
                   </select>
                 </div>
+                <div>
+                  <label for="price" className="block mb-2 text-sm font-medium text-white dark:text-dark">Numero de cuartos</label>
+                  <input type="number" name="price" id="price" className="input-text focus:ring-primary-600 focus:border-primary-600" placeholder="3" required="" />
+                </div>
+                <div>
+                  <label for="price" className="block mb-2 text-sm font-medium text-white dark:text-dark">Numero de baños</label>
+                  <input type="number" name="price" id="price" className="input-text focus:ring-primary-600 focus:border-primary-600" placeholder="2" required="" />
+                </div>
+                <div>
+                  <label for="price" className="block mb-2 text-sm font-medium text-white dark:text-dark">Maximo numero de huespedes</label>
+                  <input type="number" name="price" id="price" className="input-text focus:ring-primary-600 focus:border-primary-600" placeholder="6" required="" />
+                </div>
                 <div className="sm:col-span-2">
-                  <label for="description" className="block mb-2 text-sm font-medium text-white dark:text-dark">Description</label>
-                  <textarea id="description" rows="4" className="block p-2.5 w-full text-sm text-white bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Write product description here"></textarea>
+                  <label for="description" className="block mb-2 text-sm font-medium text-white dark:text-dark">Descripcion</label>
+                  <textarea id="description" rows="4" className="block p-2.5 w-full text-sm text-white bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-dark dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Escribe una descripcion del lugar aqui"></textarea>
                 </div>
               </div>
               <button type="submit" className="text-white inline-flex items-center focus:ring-4 focus:outline-none font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 border border-blue-900 hover:bg-blue-900">
