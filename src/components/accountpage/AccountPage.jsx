9d0a4ac09182a5007568ac6d17da012e63ee54f7
Yaxkin121 <ycardenas13@ucol.mx>
2023-04-17 20:05:08 -0600

    Progress v2

--- src/components/accountpage/AccountPage.jsx
+++ src/components/accountpage/AccountPage.jsx
@@ -1,8 +1,28 @@
 export default function accountpage() {
   return (
     <>
-      <div>
-        
+      <div className="top">
+        <div className="">Cuenta</div>
+        <div className="">Juan Gilberto Mejia Carrazco, rkrzkols@gmail.com <button>Ir a tu perfil</button></div>
+      </div>
+      <div className="mid">
+
+        <div class="grid grid-cols-4 gap-4">
+          <div className="">01</div>
+          <div className="">02</div>
+          <div className="">03</div>
+          <div className="">04</div>
+          <div className="">05</div>
+          <div className="">06</div>
+          <div className="">07</div>
+          <div className="">08</div>
+          <div className="">09</div>
+          <div className="">10</div>
+        </div>
+        <div className="">
+          <div className="">¿Necesitas desactivar tu cuenta?</div>
+          <div><button>¡Encárgate de eso ahora!</button></div>
+        </div>
       </div>
     </>
   );
