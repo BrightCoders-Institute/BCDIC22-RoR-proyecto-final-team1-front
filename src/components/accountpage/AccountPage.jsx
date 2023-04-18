6699703a07f7e0096c0ea08d34773519da3ac22a
Yaxkin121 <ycardenas13@ucol.mx>
2023-04-17 21:09:57 -0600

    Progress v3

--- src/components/accountpage/AccountPage.jsx
+++ src/components/accountpage/AccountPage.jsx
@@ -1,3 +1,7 @@
+import React from 'react'
+import './AccountPage.css'
+
+
 export default function accountpage() {
   return (
     <>
@@ -6,9 +10,8 @@ export default function accountpage() {
         <div className="">Juan Gilberto Mejia Carrazco, rkrzkols@gmail.com <button>Ir a tu perfil</button></div>
       </div>
       <div className="mid">
-
-        <div class="grid grid-cols-4 gap-4">
-          <div className="">01</div>
+        <div class="grid grid-cols-3 gap-4">
+          <div className="bg-cyan-500 drop-shadow-md rounded-lg text-center">01</div>
           <div className="">02</div>
           <div className="">03</div>
           <div className="">04</div>
@@ -19,11 +22,11 @@ export default function accountpage() {
           <div className="">09</div>
           <div className="">10</div>
         </div>
-        <div className="">
-          <div className="">¿Necesitas desactivar tu cuenta?</div>
-          <div><button>¡Encárgate de eso ahora!</button></div>
         </div>
-      </div>
+        <div className="bott">
+          <div className="">¿Necesitas desactivar tu cuenta?</div>
+          <div className=""><button>¡Encárgate de eso ahora!</button></div>
+        </div>
     </>
   );
 }
