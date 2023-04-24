11c4392852aecdc86be82b2ce0071f440f0a59ff
Reiyirak <pdsmej@gmail.com>
2023-04-24 01:20:05 -0600

    Finished design, now its time to get dynamic data

--- src/components/userplaces/Userplaces.jsx
+++ src/components/userplaces/Userplaces.jsx
@@ -1,4 +1,8 @@
 import { useState } from 'react'
+import { AiOutlinePlus } from 'react-icons/ai'
+import { FiEdit } from 'react-icons/fi'
+import { AiFillEye } from 'react-icons/ai'
+import { MdDeleteForever } from 'react-icons/md'
 import CreatePlaceForm from '../../components/createplaceform/CreatePlaceForm';
 
 const Userplaces = () => {
@@ -11,52 +15,51 @@ const Userplaces = () => {
   return (
     <>
       <section className="bg-gray-50 p-3 sm:p-5 antialiased">
-        <div className="mx-auto max-w-screen-xl px-4 lg:px-12">
-          <div className="bg-white relative shadow-md sm:rounded-lg overflow-hidden">
-            <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-              <div className="w-full md:w-1/2">
-                <form className="flex items-center">
-                  <label for="simple-search" className="sr-only">Search</label>
-                  <div className="relative w-full">
-                    <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                      <svg aria-hidden="true" className="w-5 h-5 text-gray-500" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd" />
-                      </svg>
-                    </div>
-                    <input type="text" id="simple-search" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-10 p-2" placeholder="Search" required="" />
-                  </div>
-                </form>
-              </div>
+        <div className="mx-auto max-w-screen-xl px-4">
+          <div className="bg-white relative shadow-lg sm:rounded-lg overflow-hidden">
+            <div className="flex flex-col md:flex-row items-center justify-end space-y-3 md:space-y-0 md:space-x-4 p-4">
               <div className="w-full md:w-auto flex flex-col md:flex-row space-y-2 md:space-y-0 items-stretch md:items-center justify-end md:space-x-3 flex-shrink-0">
-                <button type="button" id="createProductModalButton" data-modal-target="createProductModal" data-modal-toggle="createProductModal" className="flex items-center justify-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 focus:outline-none">
-                  <svg className="h-3.5 w-3.5 mr-2" fill="currentColor" viewbox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                    <path clip-rule="evenodd" fill-rule="evenodd" d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" />
-                  </svg>
+                <button type="button" id="createProductModalButton" onClick={() => setIsFormActive(true)} className="flex items-center justify-center text-black bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 focus:outline-none">
+                  <AiOutlinePlus className='h-4 w-4 mr-2' />
                   Add product
                 </button>
+                {isFormActive && (
+                  <CreatePlaceForm onStateChange={handleStateChange} />
+                )}
               </div>
             </div>
             <div className="overflow-x-auto">
               <table className="w-full text-sm text-left text-gray-500">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50">
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 border border-b-gray-400">
                   <tr>
-                    <th scope="col" className="px-4 py-4">Product name</th>
-                    <th scope="col" className="px-4 py-3">Category</th>
-                    <th scope="col" className="px-4 py-3">Brand</th>
-                    <th scope="col" className="px-4 py-3">Description</th>
-                    <th scope="col" className="px-4 py-3">Price</th>
-                    <th scope="col" className="px-4 py-3">
-                      <span className="sr-only">Actions</span>
-                    </th>
+                    <th scope="col" className="px-4 py-4">Nombre</th>
+                    <th scope="col" className="px-4 py-3">Prec. x noche</th>
+                    <th scope="col" className="px-4 py-3">Ciudad</th>
+                    <th scope="col" className="px-4 py-3">Num. de cuartos</th>
+                    <th scope="col" className="px-4 py-3">Num. de baños</th>
+                    <th scope="col" className="px-4 py-3">Max. num. de huespedes</th>
+                    <th scope="col" className="px-4 py-3">Latitud</th>
+                    <th scope="col" className="px-4 py-3">Longitud</th>
+                    <th scope="col" className="px-4 py-3">Descripcion</th>
+                    <th scope="col" className="px-4 py-3">Acciones</th>
                   </tr>
                 </thead>
                 <tbody>
                   <tr className="border-b">
                     <th scope="row" className="px-4 py-3 font-medium text-gray-900 whitespace-nowrap">Apple iMac 27&#34;</th>
-                    <td className="px-4 py-3">PC</td>
+                    <td className="px-4 py-3 max-w-[12rem] truncate">PC</td>
                     <td className="px-4 py-3">Apple</td>
-                    <td className="px-4 py-3 max-w-[12rem] truncate">What is a product description? A product description describes a product.</td>
+                    <td className="px-4 py-3">jiidwq</td>
                     <td className="px-4 py-3">$2999</td>
+                    <td className="px-4 py-3">PC</td>
+                    <td className="px-4 py-3 max-w-[12rem] truncate">Apple</td>
+                    <td className="px-4 py-3 max-w-[12rem] truncate">idje</td>
+                    <td className="px-4 py-3 max-w-[12rem] truncate">$2999</td>
+                    <td className="px-4 py-3 flex flex-row justify-evenly">
+                      <button type="button"><FiEdit className='h-4 w-4' /></button>
+                      <button type='button'><AiFillEye className='h-4 w-4' /></button>
+                      <button type='button'><MdDeleteForever className='h-4 w-4' /></button>
+                    </td>
                   </tr>
                 </tbody>
               </table>
@@ -64,14 +67,6 @@ const Userplaces = () => {
           </div>
         </div>
       </section>
-      <div className="flex justify-center m-5 mb-32">
-        <button id="defaultModalButton" data-modal-toggle="defaultModal" className="text-dark bg-primary-700 focus:ring-primary-300" type="button" onClick={() => setIsFormActive(true)}>
-          Create place
-        </button>
-        {isFormActive && (
-          <CreatePlaceForm onStateChange={handleStateChange} />
-        )}
-      </div>
     </>
   )
 }
